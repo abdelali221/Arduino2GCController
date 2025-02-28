88b0f1716cced20a8c9ecd95619a8494fc662a49
abdelali221 <abdelalibouyahiaoui200@gmail.com>
2025-02-28 12:24:04 +0100

    Create ARDUINO2GCPORT.ino

---
+++ ARDUINO2GCPORT.ino
@@ -0,0 +1,111 @@
+#include <Nintendo.h>
+
+CGamecubeConsole GamecubeConsole(5);
+Gamecube_Data_t d = defaultGamecubeData;
+
+int buttonAstate;
+int buttonBstate;
+int buttonUpState;
+int buttonDownState;
+int buttonStartState;
+
+int lastButtonAstate;
+int lastButtonBstate;
+int lastButtonUpState;
+int lastButtonDownState;
+int lastButtonStartState;
+
+unsigned long timer;
+char chr;
+
+bool ok = false;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("Waiting for Action");
+  if (Serial.available()) {
+    chr = Serial.read();
+  }
+  
+  switch (chr) {
+    case 'A':
+      d.report.a = HIGH;
+      Serial.println(chr);
+    break;
+
+    case 'B':
+      d.report.b = HIGH;
+      Serial.println(chr);
+    break;
+
+    case 'X':
+      d.report.x = HIGH;
+      Serial.println(chr);
+    break;
+
+    case 'Y':
+      d.report.y = HIGH;
+      Serial.println(chr);
+    break;
+
+    case 'Z':
+      d.report.z = HIGH;
+      Serial.println(chr);
+    break;
+
+    case 'S':
+      d.report.start = HIGH;
+      Serial.println(chr);
+    break;
+
+    case 'R':
+      d.report.r = HIGH;
+      Serial.println(chr);
+    break;
+
+    case 'L':
+      d.report.l = HIGH;
+      Serial.println(chr);
+    break;
+
+    case 'l':
+      d.report.dleft = HIGH;
+      Serial.println(chr);
+    break;
+
+    case 'r':
+      d.report.dright = HIGH;
+      Serial.println(chr);
+    break;
+
+    case 'u':
+      d.report.dup = HIGH;
+      Serial.println(chr);
+    break;
+
+    case 'd':
+      d.report.ddown = HIGH;
+      Serial.println(chr);
+    break;
+  }
+  GamecubeConsole.write(d);
+  
+  d.report.a = LOW;
+  d.report.b = LOW;
+  d.report.x = LOW;
+  d.report.y = LOW;
+  d.report.z = LOW;
+  d.report.start = LOW;
+  d.report.r = LOW;
+  d.report.l = LOW;
+  d.report.dleft = LOW;
+  d.report.dright = LOW;
+  d.report.dup = LOW;
+  d.report.ddown = LOW;
+  d.report.xAxis = 128;
+  d.report.yAxis = 128;
+
+}
